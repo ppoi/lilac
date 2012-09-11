9a93955159415ff57cf76be2f83e60f15ffd7609
ppoi <ppoi@tsukuba-bunko.org>
2012-09-11 20:33:48 +0900

    [機能 #56] ImportBibliographyHelperImplのテストケースがNGになっていたのを修正
    * 著者ロールをenumで比較するように修正
    * 著者ロールを一通りチェックするテストケース追加
    
    see #56

--- lilac-api/src/test/java/org/tsukuba_bunko/lilac/helper/port/impl/ImportBibliographyHelperImplTest.java
+++ lilac-api/src/test/java/org/tsukuba_bunko/lilac/helper/port/impl/ImportBibliographyHelperImplTest.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.seasar.framework.unit.Seasar2;
+import org.tsukuba_bunko.lilac.entity.AuthorRole;
 import org.tsukuba_bunko.lilac.entity.BibAuthor;
 import org.tsukuba_bunko.lilac.entity.Bibliography;
 
@@ -64,17 +65,17 @@ public class ImportBibliographyHelperImplTest {
 		assertNotNull(bibliography.authors);
 		assertEquals(3, bibliography.authors.size());
 		BibAuthor bibauthor = bibliography.authors.get(0);
-		assertEquals("author", bibauthor.authorRole);
+		assertEquals(AuthorRole.author, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertEquals((Integer)170, bibauthor.author.id);
 		assertEquals("むらさきゆきや", bibauthor.author.name);
 		bibauthor = bibliography.authors.get(1);
-		assertEquals("illustrator", bibauthor.authorRole);
+		assertEquals(AuthorRole.illustrator, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertEquals((Integer)169, bibauthor.author.id);
 		assertEquals("むにゅう", bibauthor.author.name);
 		bibauthor = bibliography.authors.get(2);
-		assertEquals("illustrator", bibauthor.authorRole);
+		assertEquals(AuthorRole.illustrator, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertEquals((Integer)104, bibauthor.author.id);
 		assertEquals("しゅがーピコラ", bibauthor.author.name);
@@ -105,20 +106,71 @@ public class ImportBibliographyHelperImplTest {
 		assertNotNull(bibliography.authors);
 		assertEquals(3, bibliography.authors.size());
 		BibAuthor bibauthor = bibliography.authors.get(0);
-		assertEquals("author", bibauthor.authorRole);
+		assertEquals(AuthorRole.author, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertNull(bibauthor.author.id);
 		assertEquals("むらさきゆきや", bibauthor.author.name);
 		bibauthor = bibliography.authors.get(1);
-		assertEquals("illustrator", bibauthor.authorRole);
+		assertEquals(AuthorRole.illustrator, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertEquals((Integer)169, bibauthor.author.id);
 		assertEquals("むにゅう", bibauthor.author.name);
 		bibauthor = bibliography.authors.get(2);
-		assertEquals("illustrator", bibauthor.authorRole);
+		assertEquals(AuthorRole.illustrator, bibauthor.authorRole);
 		assertNotNull(bibauthor.author);
 		assertNull(bibauthor.author.id);
 		assertEquals("しゅがーピコラ", bibauthor.author.name);
+	}
+
+	@Test
+	public void testRecordToBook_authorRoles() {
+		Map<String, String> record = new java.util.HashMap<String, String>();
+		record.put("status", "M");
+		record.put("id", "39");
+		record.put("isbn", "827-3939393939");
+		record.put("title", "miku");
+		record.put("subtitle", "negi");
+		record.put("price", "390");
+		record.put("publicationDate", "2007/08/27");
+		record.put("authors", "[author]むらさきゆきや\r\n(169)[illustrator]むにゅう\r\n[originator]原作C\r\n[editor]編集A\r\n[supervisor]監修B");
+		record.put("bookshelfs", "[ppoi]S99");
+
+		Bibliography bibliography = helper.recordToEntity(record);
+		assertNotNull("bibliography", bibliography);
+		assertEquals("id", (Integer)39, bibliography.id);
+		assertEquals("isbn", "827-3939393939", bibliography.isbn);
+		assertEquals("title", "miku", bibliography.title);
+		assertEquals("subtitle", "negi", bibliography.subtitle);
+		assertEquals("price", (Integer)390, bibliography.price);
+		assertEquals("isbn", "2007/08/27", new SimpleDateFormat("yyyy/MM/dd").format(bibliography.publicationDate));
+
+		assertNotNull(bibliography.authors);
+		assertEquals(5, bibliography.authors.size());
+		BibAuthor bibauthor = bibliography.authors.get(0);
+		assertEquals(AuthorRole.author, bibauthor.authorRole);
+		assertNotNull(bibauthor.author);
+		assertNull(bibauthor.author.id);
+		assertEquals("むらさきゆきや", bibauthor.author.name);
+		bibauthor = bibliography.authors.get(1);
+		assertEquals(AuthorRole.illustrator, bibauthor.authorRole);
+		assertNotNull(bibauthor.author);
+		assertEquals((Integer)169, bibauthor.author.id);
+		assertEquals("むにゅう", bibauthor.author.name);
+		bibauthor = bibliography.authors.get(2);
+		assertEquals(AuthorRole.originator, bibauthor.authorRole);
+		assertNotNull(bibauthor.author);
+		assertNull(bibauthor.author.id);
+		assertEquals("原作C", bibauthor.author.name);
+		bibauthor = bibliography.authors.get(3);
+		assertEquals(AuthorRole.editor, bibauthor.authorRole);
+		assertNotNull(bibauthor.author);
+		assertNull(bibauthor.author.id);
+		assertEquals("編集A", bibauthor.author.name);
+		bibauthor = bibliography.authors.get(4);
+		assertEquals(AuthorRole.supervisor, bibauthor.authorRole);
+		assertNotNull(bibauthor.author);
+		assertNull(bibauthor.author.id);
+		assertEquals("監修B", bibauthor.author.name);
 	}
 
 }
